6bc77280fb1eff8abbe9dfa6e9bbd56d1e6bfc56
Muzaffer Tozduman <tozduman@hotmail.com>
2021-02-11 01:20:23 +0300

    20210211 Lessons

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<E> where E:class,IEntity, new()
+    {
+        List<E> GetAll(Expression<Func<E,bool>> filter=null);
+        E Get(Expression<Func<E, bool>> filter);
+        void Add(E entity);
+        void Update(E entity);
+        void Delete(E entity);
+        // List<E> GetAllByCategory(int categoryId);
+    }
+}
